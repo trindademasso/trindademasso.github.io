283fcbabc3e67e3f26279237237bb653a5e9ef88
luishenriquetp <luishenriquetp@gmail.com>
2025-04-23 19:54:57 -0300

    Production

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import SectionOne from "@/app/components/SectionOne";
+import SectionTwo from "@/app/components/SectionTwo";
+import SectionThree from "@/app/components/SectionThree";
+import SectionFour from "@/app/components/SectionFour";
+import SectionFive from "@/app/components/SectionFive";
+
+
 export default function Home() {
   return (
-    <main>
-      <div>Next.js on GitHub Pages</div>
-    </main>
+    <>
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true, // ✅ desativa a otimização
+  },
+
 };
 
 export default nextConfig;

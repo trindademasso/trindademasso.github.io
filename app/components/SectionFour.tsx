1bedaceffd8d72c5771d7f614b3d79b39867e6ee
luishenriquetp <luishenriquetp@gmail.com>
2025-04-23 21:05:43 -0300

    Production

--- app/components/SectionFour.tsx
+++ app/components/SectionFour.tsx
@@ -15,7 +15,7 @@ export default function SectionFour() {
           <div className="text-center">
             <div className="flex justify-center items-center pb-8">
               <Image
-                src="/trindade2/mogi.png"
+                src="/trindade/mogi.png"
                 alt="Ícone da Reidente"
                 width={440}
                 height={440}
@@ -31,7 +31,7 @@ export default function SectionFour() {
           <div className="text-center">
             <div className="flex justify-center items-center pb-8">
               <Image
-                src="/trindade2/sp.png"
+                src="/trindade/sp.png"
                 alt="Ícone da Reidente"
                 width={440}
                 height={440}
